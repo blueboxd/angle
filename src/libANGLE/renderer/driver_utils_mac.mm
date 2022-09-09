344c3682aa8e3f0185328b62d01310f117c5ad1c
bluebox <bluebox@iMac-pro.local>
2022-09-09 15:07:10 +0900

    for master.lion

--- src/libANGLE/renderer/driver_utils_mac.mm
+++ src/libANGLE/renderer/driver_utils_mac.mm
@@ -17,12 +17,17 @@ namespace rx
 OSVersion GetMacOSVersion()
 {
     OSVersion result;
-
-    NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
-    result.majorVersion              = static_cast<int>(version.majorVersion);
-    result.minorVersion              = static_cast<int>(version.minorVersion);
-    result.patchVersion              = static_cast<int>(version.patchVersion);
-
+    if (@available(macOS 10.10, *)) {
+      NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+      result.majorVersion              = static_cast<int>(version.majorVersion);
+      result.minorVersion              = static_cast<int>(version.minorVersion);
+      result.patchVersion              = static_cast<int>(version.patchVersion);
+    } else {
+      extern OSErr Gestalt(OSType selector, SInt32 *response) __attribute__((weak_import, weak));
+      ::Gestalt(gestaltSystemVersionMajor, reinterpret_cast<SInt32*>(&result.majorVersion));
+      ::Gestalt(gestaltSystemVersionMinor, reinterpret_cast<SInt32*>(&result.minorVersion));
+      ::Gestalt(gestaltSystemVersionBugFix, reinterpret_cast<SInt32*>(&result.patchVersion));
+    }
     return result;
 }
 #endif
